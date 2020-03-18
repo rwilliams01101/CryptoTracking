5a9627e41b093c9d40daf81011429d9cf5f60506
RAW <rwilliams01101@gmail.com>
2020-03-18 11:45:33 -0500

    delete button click event working, removed various unused comments

--- buildcoins.js
+++ buildcoins.js
@@ -1,30 +1,12 @@
-/* FOR REFERECE ONLY. Once index.html is updated, will use id/classes accordingly
+/* FOR REFERECE ONLY. 
 
-    Add dynamic HTML to aggregate the coin.
+Add dynamic HTML to aggregate the coin.
 
 Add cards for each coin based on data in local storage.
 Add delete button that will remove from local storage and page.
 Display information on card
 Add data to card.
-Add delete button on card
-
-
-<div class="container-fluid">
-<div class="row">
-  <!-- I question the "aside" element here for web accessibiliy reasons, maybe list as "section" instead -->
-  <aside class="col-lg-3 bg-light">
-    <!-- small header above search input -->
-    <h5 class="mt-1">Search for a City:</h5>
-    <div class="form-inline form-group">
-      <!-- input field -->
-      <input class="form-control" type="text" id="search-value" />
-      <!-- search button -->
-      <button class="btn btn-primary" id="search-button"> <i class="fa fa-search"></i> </button>
-      <button id="clrBtn">Clear</button>
-    </div>
-    <!-- beginning of unordered list -->
-    <ul class="list-group history"></ul>
-  </aside> */
+Add delete button on card */
 
 function buildCard() {
   // var {
@@ -42,11 +24,14 @@ function buildCard() {
   var cardDiv = $("<div>").addClass("col-sm-3");
 
   var cardDivHeader = $("<div>")
-    .addClass("card-header text-center")
+    .addClass("card-header")
     .text(coin_name);
 
   // this is a placeholder until API data becomes available. Will save all icons in "Assets" folder
-  var img =$("<img>").attr("src", "http://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/48/NEO-icon.png");
+  var img = $("<img>").attr(
+    "src",
+    "http://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/48/NEO-icon.png"
+  );
 
   var cardDivBody = $("<P>")
     .addClass("card-text")
@@ -54,32 +39,27 @@ function buildCard() {
 
   var coinVolume = $("<div>").text("Volume: 15000");
 
-  var deleteBtn = $("<div>").html("<button>Delete</button>");
+  var deleteBtn = $("<div>").html("<button id=deleteBtn>Delete</button>");
 
   // type="button" class="btn btn-outline-dark"
   // .text("Price: " + formatter.format(last_price)); <<<< this was line 73
   // cardDivH5 = $("<h5>");
   // cardDivH5.addClass("card-title");
 
-  cardDiv.append(
-    cardDivHeader,
-    img,
-    cardDivBody,
-    coinVolume,
-    deleteBtn);
+  cardDiv.append(cardDivHeader, img, cardDivBody, coinVolume, deleteBtn);
 
   $("#cardRender").append(cardDiv);
 }
 
 // Clear button removes all items in "col-sm-3" class
 $("#clrBtn").on("click", function() {
-  console.log("Clearworks")
   $(".col-sm-3").remove();
 });
 
 // TODO: Still developing deleteBtn functionality, need to get unique ids to generate for cards
-$("#deleteBtn").on("click", function() {
-  console.log("deleteworks")
+$(document).on("click", '#deleteBtn', function() {
+
+  console.log("delete is working");
 });
 
 // Event listener for search button
