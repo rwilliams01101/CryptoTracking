3bc5db582a87ade483a3b91f84e62a7a294cccf0
srfrog <shawn.hayes70@gmail.com>
2020-03-23 10:52:29 -0500

    Fixed Delete

--- coindeskAPI.js
+++ coindeskAPI.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
-  var coinList = JSON.parse(window.localStorage.getItem("coins")) || [];
-  coinList.forEach(loadHistData);
-});
+// $(document).ready(function() {
+//   var coinList = JSON.parse(window.localStorage.getItem("coins")) || [];
+//   coinList.forEach(loadHistData);
+// });
 
-function loadHistData(coin) {
-  var queryURL =
-    "https://min-api.cryptocompare.com/data/v2/histoday?fsym=" +
-    coin +
-    "&tsym=USD&limit=10&api_key=d797606d445e37e288f15360b88c112e5f8629b3437e482303471a25b1785e78";
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    // console.log(response);
-  });
-}
+// function loadHistData(coin) {
+//   var queryURL =
+//     "https://min-api.cryptocompare.com/data/v2/histoday?fsym=" +
+//     coin +
+//     "&tsym=USD&limit=10&api_key=d797606d445e37e288f15360b88c112e5f8629b3437e482303471a25b1785e78";
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+//     // console.log(response);
+//   });
+// }
